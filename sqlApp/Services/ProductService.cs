db2b9f0ae13dd01e0bf90006ae64d9ed1b98f7e3
AliK01 <AliK01@PICKLES-PC>
2023-02-24 20:36:40 +0800

    Add project files.

---
+++ sqlApp/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using sqlApp.Models;
+using System.Data.SqlClient;
+
+namespace sqlApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "homoro.database.windows.net";
+        private static string db_user = "captain";
+        private static string db_password = "azure@123";
+        private static string db_database = "captain";
+
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+}
